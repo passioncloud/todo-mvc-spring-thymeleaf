bc72d24a5a936a4eec5cfb192d2d7712fc7ec3b0
Gilbert <gssenyonjo@gmail.com>
2022-04-23 17:11:33 +0300

    Setup todo page with todomvc styles imported using webjars

--- src/main/java/net/passioncloud/todo/controller/TodoItemController.java
+++ src/main/java/net/passioncloud/todo/controller/TodoItemController.java
@@ -1,6 +1,8 @@
 package net.passioncloud.todo.controller;
 
+import net.passioncloud.todo.repository.TodoItemRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class TodoItemController {
 
+    private final TodoItemRepository todoItemRepository;
+
+    // inject TodoItemRepository via constructor injection
+    public TodoItemController(TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("totalNumberOfItems", todoItemRepository.count());
+        model.addAttribute("someItemsPresent", todoItemRepository.count() != 0);
         return "index";
     }
 }
