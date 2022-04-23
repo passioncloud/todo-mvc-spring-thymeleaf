434f6aa36d16bd9c2d0100b457a386f017d32e2f
Gilbert <gssenyonjo@gmail.com>
2022-04-23 18:38:01 +0300

    Completed part one of todomvc https://www.wimdeblauwe.com/blog/2021/09/20/todomvc-with-spring-boot-and-thymeleaf-part-1/

--- src/main/java/net/passioncloud/todo/controller/TodoItemController.java
+++ src/main/java/net/passioncloud/todo/controller/TodoItemController.java
@@ -1,10 +1,18 @@
 package net.passioncloud.todo.controller;
 
+import net.passioncloud.todo.form.TodoItemFormData;
+import net.passioncloud.todo.models.TodoItem;
 import net.passioncloud.todo.repository.TodoItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -19,8 +27,33 @@ public class TodoItemController {
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("totalNumberOfItems", todoItemRepository.count());
         model.addAttribute("someItemsPresent", todoItemRepository.count() != 0);
+        model.addAttribute("item", new TodoItemFormData());
+        model.addAttribute("todos", getTodoItems());
         return "index";
     }
+
+    private List<TodoItemDto> getTodoItems() {
+        return todoItemRepository.findAll()
+                .stream()
+                .map(todoItem -> new TodoItemDto(
+                        todoItem.getId(),
+                        todoItem.getTitle(),
+                        todoItem.isCompleted()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public record TodoItemDto(long id, String title, boolean isCompleted) {}
+
+
+
+    @PostMapping
+    public String addNewTodoItem(@Valid @ModelAttribute("item") TodoItemFormData todoItemFormData) {
+        var todoItem = new TodoItem();
+        todoItem.setTitle(todoItemFormData.getTitle());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
 }
+
---
+++ src/main/java/net/passioncloud/todo/form/TodoItemFormData.java
@@ -0,0 +1,16 @@
+package net.passioncloud.todo.form;
+
+import javax.validation.constraints.NotBlank;
+
+public class TodoItemFormData {
+    @NotBlank
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
