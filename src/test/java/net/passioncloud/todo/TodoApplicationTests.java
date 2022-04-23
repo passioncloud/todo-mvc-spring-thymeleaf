27f393e82d9134adb460dfd60a856fc3302e7342
Gilbert <gssenyonjo@gmail.com>
2022-04-23 13:01:11 +0300

    Initial commit

---
+++ src/test/java/net/passioncloud/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.passioncloud.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
