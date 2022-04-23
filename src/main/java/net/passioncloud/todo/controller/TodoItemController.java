0a19cdc48bce3128437179ee09154212e0430c37
Gilbert <gssenyonjo@gmail.com>
2022-04-23 21:35:53 +0300

    Implemented filtering completed and incompleted. Implemented clearing completed todos. Finished 3 part series.

--- src/main/java/net/passioncloud/todo/controller/TodoItemController.java
+++ src/main/java/net/passioncloud/todo/controller/TodoItemController.java
@@ -5,12 +5,10 @@ import net.passioncloud.todo.models.TodoItem;
 import net.passioncloud.todo.repository.TodoItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,31 +18,60 @@ public class TodoItemController {
 
     private final TodoItemRepository todoItemRepository;
 
+    public enum ListFilter { ALL, INCOMPLETE, COMPLETED };
+
     // inject TodoItemRepository via constructor injection
     public TodoItemController(TodoItemRepository todoItemRepository) {
         this.todoItemRepository = todoItemRepository;
     }
 
-    @GetMapping
-    public String index(Model model) {
+    private void addAttributesForIndex(Model model, ListFilter listFilter) {
         model.addAttribute("someItemsPresent", todoItemRepository.count() != 0);
         model.addAttribute("item", new TodoItemFormData());
-        model.addAttribute("todos", getTodoItems());
+        model.addAttribute("todos", getTodoItems(listFilter));
+        model.addAttribute("incompleteTodoItemCount", todoItemRepository.countAllByCompleted(false));
+        model.addAttribute("todoItemCount", todoItemRepository.count());
+        model.addAttribute("listFilter", listFilter);
+        model.addAttribute("someCompletedTodosPresent", todoItemRepository.existsTodoItemByCompleted(true));
+    }
+    @GetMapping("/")
+    public String index(Model model) {
+        addAttributesForIndex(model, ListFilter.ALL);
         return "index";
     }
 
-    private List<TodoItemDto> getTodoItems() {
-        return todoItemRepository.findAll()
+    @GetMapping("/completed")
+    public String completed(Model model) {
+        addAttributesForIndex(model, ListFilter.COMPLETED);
+        return "index";
+    }
+    @GetMapping("/incomplete")
+    public String incomplete(Model model){
+        addAttributesForIndex(model, ListFilter.INCOMPLETE);
+        return "index";
+    }
+
+    private List<TodoItemDto> getTodoItems(ListFilter listFilter) {
+        List<TodoItem> todoItems = switch (listFilter) {
+            case ALL -> todoItemRepository.findAll();
+            case COMPLETED -> todoItemRepository.findAllByCompleted(true);
+            case INCOMPLETE -> todoItemRepository.findAllByCompleted(false);
+        };
+        return todoItems
                 .stream()
-                .map(todoItem -> new TodoItemDto(
-                        todoItem.getId(),
-                        todoItem.getTitle(),
-                        todoItem.isCompleted()
-                ))
+                .map(TodoItemDto::fromTodoItem)
                 .collect(Collectors.toList());
     }
 
-    public record TodoItemDto(long id, String title, boolean isCompleted) {}
+    public record TodoItemDto(long id, String title, boolean isCompleted) {
+        static TodoItemDto fromTodoItem(TodoItem todoItem) {
+            return new TodoItemDto(
+                    todoItem.getId(),
+                    todoItem.getTitle(),
+                    todoItem.isCompleted()
+            );
+        }
+    }
 
 
 
@@ -53,6 +80,26 @@ public class TodoItemController {
         var todoItem = new TodoItem();
         todoItem.setTitle(todoItemFormData.getTitle());
         todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
+
+    @PutMapping("/{id}/toggle")
+    public String toggleSelection(@PathVariable("id") Long id) {
+        TodoItem todoItem = todoItemRepository.getById(id);
+        todoItem.setCompleted(!todoItem.isCompleted());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTodoItem(@PathVariable("id") Long id) {
+        todoItemRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/completed")
+    public String deleteCompletedTodoItems() {
+        todoItemRepository.deleteAll(todoItemRepository.findAllByCompleted(true));
         return "redirect:/";
     }
 }
--- src/main/java/net/passioncloud/todo/repository/TodoItemRepository.java
+++ src/main/java/net/passioncloud/todo/repository/TodoItemRepository.java
@@ -3,5 +3,10 @@ package net.passioncloud.todo.repository;
 import net.passioncloud.todo.models.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+    int countAllByCompleted(boolean completed);
+    List<TodoItem> findAllByCompleted(boolean completed);
+    boolean existsTodoItemByCompleted(boolean completed);
 }
